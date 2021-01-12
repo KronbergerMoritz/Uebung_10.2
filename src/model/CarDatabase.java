7803befd23e3b575c3d2b636318bb76b602ec3a7
Moritz Kronberger <moritzkronberger@aon.at>
2021-01-12 17:07:56 +0100

    Starten Search

--- src/model/CarDatabase.java
+++ src/model/CarDatabase.java
@@ -1,4 +1,14 @@
 package model;
 
+import java.util.HashMap;
+
 public class CarDatabase {
+
+    protected HashMap db;
+
+    public Vehicle search(String licencePlate, boolean exact)
+    {
+
+        return null;
+    }
 }
--- src/model/Vehicle.java
+++ src/model/Vehicle.java
@@ -1,4 +1,57 @@
 package model;
 
 public class Vehicle {
+
+    public Vehicle() {
+    }
+
+private String color;
+private String owner;
+private String manufacturer;
+private String model;
+private String licensePlate;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+    public String toString()
+    {
+        return "hello World";
+    }
 }
